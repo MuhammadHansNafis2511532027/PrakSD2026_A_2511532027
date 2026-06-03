ef57c559a2dfb649b72835ece8e156cb679a9a4b
MuhammadHansNafis2511532027 <2511532027_muhammad@student.unand.ac.id>
2026-06-03 20:21:34 +0700

    Tugas Praktikum Pekan_8_2511532027_Muhammad Hans Nafis_A

---
+++ PrakSD2026_A_2511532027/src/pekan8_2511532027/Lagu_2511532027.java
@@ -0,0 +1,24 @@
+package pekan8_2511532027;
+
+public class Lagu_2511532027 {
+    private String judul_2027, penyanyi_2027;
+    private int durasi_2027;
+
+    public Lagu_2511532027(String judul_2027, String penyanyi_2027, int durasi_2027) {
+        this.judul_2027 = judul_2027;
+        this.penyanyi_2027 = penyanyi_2027;
+        this.durasi_2027 = durasi_2027;
+    }
+
+    public String getJudul_2027() {
+        return judul_2027;
+    }
+
+    public String getPenyanyi_2027() {
+        return penyanyi_2027;
+    }
+
+    public int getDurasi_2027() {
+        return durasi_2027;
+    }
+}
